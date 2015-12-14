9b6a0b6bf67bf69888d393b6a25807eaa0e234f8
Batze <heavydutyfirecracker@googlemail.com>
2015-12-14 13:58:19 +0100

    - Abfahrtszeit von VVO Website abfragen - Ergebnis JSon Array decodieren und in list data einlesen

--- VVO.py
+++ VVO.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
+import httplib
+import json
 
+con = httplib.HTTPConnection("widgets.vvo-online.de")
+con.request("GET", "/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=Hp%20Dobritz")
+res = con.getresponse()
+data = json.loads(res.read())
+# data [0][0] Linie
+# data [0][1] Richtung
+# data [0][2] Abfahrt in Minuten
+print data[0][1]
